88c844a8fc6202aeab50d42a2885c2c48829b430
Andrii Kovalenko <a.kovalenko@celonis.com>
2023-09-29 18:38:49 +0200

    extract ConfigFactory

--- src/main/java/com/google/guava/Main.java
+++ src/main/java/com/google/guava/Main.java
@@ -10,64 +10,14 @@ import java.io.InputStream;
 import java.net.URL;
 
 public class Main {
-    private static File createdTempDir;
 
-    static {
-        createdTempDir = Files.createTempDir();
-        createTestFile();
-    }
 
     public static void main(String[] args) {
-        System.out.printf("createdTempDir is located at \"%s\"%n", createdTempDir.getAbsolutePath());
-        System.out.printf("Content: %s%n", (Object[]) createdTempDir.list());
+        final var configFactory = new ConfigFactory();
+        final var configTempDir = configFactory.tempDir();
+        System.out.printf("createdTempDir is located at \"%s\"%n", configTempDir.getAbsolutePath());
+        System.out.printf("Content: %s%n", (Object[]) configTempDir.list());
     }
 
-    private static void createTestFile() {
-        URL testFileUrl = Thread.currentThread().getContextClassLoader().getResource("test.file");
-        if (testFileUrl == null) {
-            throw new RuntimeException("Not able to find test.file");
-        } else {
 
-
-            try {
-                InputStream is = testFileUrl.openStream();
-
-                try {
-                    File testFileFile = new File(createdTempDir, "test.file");
-                    FileOutputStream os = new FileOutputStream(testFileFile);
-
-                    try {
-                        IOUtils.copy(is, os);
-                    } catch (Throwable var9) {
-                        try {
-                            os.close();
-                        } catch (Throwable var8) {
-                            var9.addSuppressed(var8);
-                        }
-
-                        throw var9;
-                    }
-
-                    os.close();
-                    testFileFile.setExecutable(true);
-                } catch (Throwable var10) {
-                    if (is != null) {
-                        try {
-                            is.close();
-                        } catch (Throwable var7) {
-                            var10.addSuppressed(var7);
-                        }
-                    }
-
-                    throw var10;
-                }
-
-                if (is != null) {
-                    is.close();
-                }
-            } catch (IOException var11) {
-                throw new RuntimeException(var11);
-            }
-        }
-    }
 }
